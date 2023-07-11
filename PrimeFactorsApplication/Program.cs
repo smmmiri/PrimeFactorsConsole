f8197291e14e687a94c8e683f6af57d468781275
Sayyed Mohammad Miri <smmmiri@outlook.com>
2023-07-11 16:19:33 +0330

    Add project files.

---
+++ PrimeFactorsApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using PrimeFactorsLibrary;
+
+string? continueInput;
+
+do
+{
+    Console.Write("Please enter a number between 2 and 1000 to displays its prime factors: ");
+    var input = Console.ReadLine();
+    Console.WriteLine();
+
+    try
+    {
+        var number = int.Parse(input);
+        if (number <= 1 || number > 1000)
+            throw new OverflowException();
+
+        PrimeFactorsCalculator.Calculate(number);
+        Console.WriteLine($"Prime factors of {number} are: {PrimeFactorsCalculator.Result}");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("The entered number must be between 2 and 1000.");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Please enter number correctly.");
+    }
+    finally
+    {
+        Console.WriteLine("\nDo you want to continue? (Y/N)");
+        continueInput = Console.ReadLine();
+        Console.WriteLine();
+    }
+} while (continueInput.ToLower() != "n");
